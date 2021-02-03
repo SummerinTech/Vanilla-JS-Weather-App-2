81eb03708282b1765322361b615e5b1fd30d0ee6
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-02-02 19:36:49 -0800

    added event listener to main city

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 function searchCity(event) {
 	event.preventDefault();
+	let searchInput = document.querySelector("#search-input");
+	let h1 = document.querySelector("h1");
+	h1.innerHTML = searchInput.value;
 }
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
 function showTemperature(response) {
-	console.log(response.data);
+	// console.log(response.data);
 	document.querySelector(".temperature").innerHTML = Math.round(
 		response.data.main.temp
 	);
